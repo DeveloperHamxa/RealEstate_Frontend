741eae459489321ea9853e02aaf2bd8527ac3484
Hamza Naveed <officialhamxa@gmail.com>
2022-08-31 04:32:29 -0700

    Frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import NavBar from './components/navbar/Navbar';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom'
+import HomePage from './components/home/Home';
+import SignUpPage from './components/SignUp/SignUp';
+import LoginPage from './components/login/Login';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+const App=()=>{
+
+    
+    return (
+        <Router>
+        <div className="">
+            <NavBar/>
+            <Switch>
+                <Route path="/login">
+                    <LoginPage/>
+                </Route>
+                <Route path="/signup">
+                    <SignUpPage/>                
+                </Route>    
+                <Route path="/">
+                    <HomePage/>
+                </Route>
+            </Switch>
+        </div>
+        </Router>
+    )
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'))
